d7abb00514a634a432929bbe74cecbf6cf595f0b
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-05-14 15:28:01 -0500

    WIP: Mission in progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+const mouse = document.querySelector('footer')
+mouse.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'pink';
+    setTimeout(function() {
+        event.target.style.backgroundColor = "";
+    }, 1000)
+}, false)
+
+const select = document.querySelector('.first-name')
+
+select.addEventListener('select', event => {
+    const log = document.getElementById('log');
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    log.textContent = `You selected: ${selection}`;
+    
+});
