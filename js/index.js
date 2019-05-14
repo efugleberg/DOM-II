011e8dbe53050aef8e4738b06a2f83e53247203d
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-05-14 17:36:29 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -63,25 +63,29 @@ function zoom(event) {
   var dragged;
   /* events fired on the draggable target */
   document.addEventListener("drag", function(event) {
-  
+    event.stopPropagation();
   }, false);
   document.addEventListener("dragstart", function(event) {
     // store a ref. on the dragged elem
+    event.stopPropagation();
     dragged = event.target;
     // make it half transparent
     event.target.style.opacity = .5;
   }, false);
   document.addEventListener("dragend", function(event) {
     // reset the transparency
+    event.stopPropagation();
     event.target.style.opacity = "";
   }, false);
   /* events fired on the drop targets */
   document.addEventListener("dragover", function(event) {
     // prevent default to allow drop
+    event.stopPropagation();
     event.preventDefault();
   }, false);
   document.addEventListener("dragenter", function(event) {
     // highlight potential drop target when the draggable element enters it
+    event.stopPropagation();
     if (event.target.className == "dropzone") {
       event.target.style.background = "purple";
     }
@@ -89,6 +93,7 @@ function zoom(event) {
   }, false);
   document.addEventListener("dragleave", function(event) {
     // reset background of potential drop target when the draggable element leaves it
+    event.stopPropagation();
     if (event.target.className == "dropzone") {
       event.target.style.background = "";
     }
@@ -96,6 +101,7 @@ function zoom(event) {
   }, false);
   document.addEventListener("drop", function(event) {
     // prevent default action (open as link for some elements)
+    event.stopPropagation();
     event.preventDefault();
     // move dragged elem to the selected drop target
     if (event.target.className == "dropzone") {
@@ -124,3 +130,46 @@ window.addEventListener('scroll', function(e) {
     ticking = true;
   }
 });
+
+const xhrButtonError = document.querySelector('.xhr.error');
+const log = document.querySelector('.event-log');
+function handleEvent(e) {
+    log.textContent = log.textContent + `${e.type}: ${e.loaded} bytes transferred\n`;
+}
+
+function addListeners(xhr) {
+    xhr.addEventListener('loadstart', handleEvent);
+    xhr.addEventListener('load', handleEvent);
+    xhr.addEventListener('loadend', handleEvent);
+    xhr.addEventListener('progress', handleEvent);
+    xhr.addEventListener('error', handleEvent);
+    xhr.addEventListener('abort', handleEvent);
+}
+
+function runXHR(url) {
+    log.textContent = '';
+    const xhr = new XMLHttpRequest();
+    addListeners(xhr);
+    xhr.open("GET", url);
+    xhr.send();
+    return xhr;  
+}
+xhrButtonError.addEventListener('click', () => {
+    runXHR('https://somewhere.org/i-dont-exist');
+});
+
+const form = document.getElementById('accountCreation');
+
+form.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';    
+}, true);
+
+form.addEventListener('blur', (event) => {
+  event.target.style.background = '';    
+}, true);
+
+const aTags = document.querySelector('nav')
+aTags.addEventListener('click', event => {
+    event.preventDefault();
+    console.log('hello')
+})
